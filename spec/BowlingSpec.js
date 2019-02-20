4cb86a63178f23f9795467b6a85f8f90deffa81c
Nick <nicnowak@nmu.edu>
2019-02-19 22:49:49 -0500

    redesigned game instantiation for test cases

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,6 +1,5 @@
 var Bowling = require("../Bowling");
 
-var g = new Bowling.Game;
 
 
 describe("Game instantiation", function() {
@@ -9,15 +8,20 @@ describe("Game instantiation", function() {
 	});
 
 	it("Gutter game should be zero", function() {
-		expect(g.roll_n_times(0, 20)).toEqual(0);
+		var g = new Bowling.Game;
+		g.roll_n_times(0, 20)
+		expect(g.score()).toEqual(0);
 	});
 	
 	it("All ones should equal 20", function() {
-		expect(g.roll_n_times(1, 20)).toEqual(20);
+		var g = new Bowling.Game;
+		g.roll_n_times(1, 20);
+		expect(g.score()).toEqual(20);
 	});
 
 	it("A spare + 3 pins, + all misses scores 24 ", function() {
-		expect(g.roll_n_times(1, 20)).toEqual(20);
+		var g = new Bowling.Game;
+		expect(g.score()).toEqual(24);
 	});
 
 });
