332069422f9ddb632fb9fdf3b061f25d6a1c403c
nick <nicholaspnowak@gmail.com>
2019-02-20 01:01:01 -0500

    Update Bowling.js

--- Bowling.js
+++ Bowling.js
@@ -15,29 +15,27 @@ var Bowling = (function () {
 
 		this.score = function(){
 			var score = 0; 
-			
 			for (var i = 0; i < rolls.length/2; i+=2){
 				var frame = [rolls[i], rolls[i+1]];
 				
+				//strike
 				if (rolls[i] == 10){
 					if (i < 10){
 						score += (10  + rolls[i+1] + rolls[i+2]);
 						i--;
 					}
-
 				}
 				
+				//spare
 				else if (frame.reduce(sum) == 10){
 					score += (10 + rolls[i+2]);
 				}
-
 				else {
 					score += (rolls[i] + rolls[i+1]);
 				}
 			}
 			return score;
 		}
-
 		this.roll_n_times = function(pins, n){
 			for (var i = 0; i < n; i++){
 				this.roll(pins);
